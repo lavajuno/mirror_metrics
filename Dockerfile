7def967d01e0380421bbe25ee86125b664f56e8d
Juno <jvmeifert@gmail.com>
2024-03-07 21:26:47 -0500

    get prometheus working

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Build
+FROM ubuntu:latest as builder
+RUN apt update && apt upgrade -y
+RUN apt install -y g++ cmake ninja-build git libzmq3-dev libcurl4-openssl-dev zlib1g-dev
+WORKDIR /mirror_metrics
+COPY ./src /mirror_metrics/src
+COPY ./lib /mirror_metrics/lib
+COPY CMakeLists.txt /mirror_metrics/CMakeLists.txt
+RUN cmake -S/mirror_metrics -B/mirror_metrics/build -G Ninja
+RUN cmake --build /mirror_metrics/build --target clean
+RUN cmake --build /mirror_metrics/build --target all
+
+# Run
+FROM ubuntu:latest
+RUN apt update && apt upgrade -y
+RUN apt install -y libzmq3-dev curl
+EXPOSE 8080
+WORKDIR /mirror_metrics
+COPY --from=builder /mirror_metrics/build/mirror_metrics_engine .
+ENTRYPOINT ["./mirror_metrics_engine"]
--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,13 @@
 #include <event/TimeStamp.hpp>
 #include <mirror/logger.hpp>
 
+#include <prometheus/counter.h>
+#include <prometheus/exposer.h>
+#include <prometheus/registry.h>
+
+#include <memory>
 #include <iostream>
+#include <thread>
 
 using namespace mirror;
 
@@ -13,6 +19,26 @@ void terminate(int signal) {
 }
 
 int main() {
+
+    prometheus::Exposer exposer{"0.0.0.0:8080"};
+
+    auto registry = std::make_shared<prometheus::Registry>();
+
+    auto& hit_counter = prometheus::BuildCounter()
+            .Name("hits")
+            .Help("Hit counter")
+            .Register(*registry);
+
+    auto& tx_counter = hit_counter.Add({{"test", "tset2"}});
+
+    exposer.RegisterCollectable(registry);
+
+    while(true) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        tx_counter.Increment();
+        std::cout << tx_counter.Value() << std::endl;
+    }
+
     std::string a = "\"07/Mar/2024:00:01:05 -0500\" \"66.203.67.100\" \"GET /opensuse/opensuse/update/leap/15.5/backports/repodata/23a22ea99529a8ab0529648312715d3ffe1252963f9f545d444c184cfaa91cd3-primary.xml.gz HTTP/1.1\" \"206\" \"169061\" \"259\" \"ZYpp 17.31.15 (curl 8.0.1)";
 
     mirror::Event ab(a);
